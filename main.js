baaea1b2cbc0c3cb71ea5cf0934ae4d840066831
EdwinDij <edwin.d899@gmail.com>
2023-04-02 22:31:32 +0200

    get ok

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ const express = require('express');
 const app =  express();
 const fs = require('fs');
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+let data = JSON.parse(fs.readFileSync('inventors.json', 'utf-8'));
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
+
+app.get('/data', (req, res) => {
+    res.send(data);
+})
+
