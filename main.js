c8650e6b215d1cfad451d35e4df347307271e120
EdwinDij <edwin.d899@gmail.com>
2023-04-02 23:15:06 +0200

    ajout de data possible

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app =  express();
+const app = express();
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-let data = JSON.parse(fs.readFileSync('inventors.json', 'utf-8'));
+const data = JSON.parse(fs.readFileSync('inventors.json', 'utf-8'));
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
@@ -14,4 +15,19 @@ app.listen(port, () => {
 app.get('/data', (req, res) => {
     res.send(data);
 })
+
+app.post('/data', (req, res) => {
+    const newItem = {
+        id: data["inventors"].length + 1,
+        name: req.body.name,
+        age: req.body.age,
+        work: req.body.work,
+        bio: req.body.bio,
+        city: req.body.city,
+    };
+    data["inventors"].push(newItem);
+    fs.writeFileSync('inventors.json', JSON.stringify(data));
+    res.send(data);
+    
+});
 
